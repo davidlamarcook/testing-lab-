abb6e9e1001eeb670565614dc6e9efe4155a6d2e
davidlamarcook <dcook1025@gmail.com>
2022-06-17 20:47:58 -0600

    describe-tests

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const {returnTwo, greeting, add} = require('./functions')
+const {returnTwo, greeting, add, multiply, divide, subtract} = require('./functions')
 
 test('returns number 2', () => {
     expect(returnTwo()).toBe(2)
@@ -10,4 +10,31 @@ test('returns greeted name', () => {
 
 test('add sum', () => {
     expect(add(3,5)).toBe(8)
+})
+
+test('multiply', () => {
+    expect(multiply(3,5)).toBe(15)
+})
+
+test('divide sum', () => {
+    expect(divide(10, 5)).toBe(2)
+})
+
+test('subtraction', () => {
+    expect(subtract(8, 3)).toBe(5)
+})
+
+describe("Math Challenges", () => {
+    test('add sum', () => {
+        expect(add(3,5)).toBe(8)
+    })
+    test('multiply', () => {
+    expect(multiply(3,5)).toBe(15)
+    })
+    test('divide sum', () => {
+        expect(divide(10, 5)).toBe(2)
+    })
+    test('subtraction', () => {
+        expect(subtract(8, 3)).toBe(5)
+    })
 })
